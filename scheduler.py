56c79a7ed24ac50206e350eeb4ccaf5a9d31494e
matej2 <11059438+matej2@users.noreply.github.com>
2021-03-14 01:36:18 +0100

    Scheduler Wiki api

---
+++ scheduler.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+from main import main
+
+if __name__ == '__main__':
+    scheduler = AsyncIOScheduler()
+
+    scheduler.add_job(main, trigger=IntervalTrigger(seconds=10))
+
+    scheduler.start()
+    # Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
